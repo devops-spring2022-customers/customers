89c8976d45151b9294bb441e375bc7d361d3e722
Allen <azflight1995@gmail.com>
2022-03-08 10:47:32 +0000

    remove weird conflict

--- tests/test_models.py
+++ tests/test_models.py
@@ -189,7 +189,6 @@ class TestCustomerModel(unittest.TestCase):
         data["addresses"] = "abc"
         customer = Customer()
         self.assertRaises(DataValidationError, customer.deserialize, data)
-<<<<<<< HEAD
     
     def test_list_all(self):
         """Test case to list all customers"""
@@ -197,7 +196,7 @@ class TestCustomerModel(unittest.TestCase):
         for customer in customers:
             customer.create()
         self.assertEqual(len(Customer.all()), 3)
-=======
+        self.assertEqual(len(Customer.all()), 3)
 
     def test_delete_a_customer(self):
         """Delete a Customer"""
@@ -217,4 +216,3 @@ class TestCustomerModel(unittest.TestCase):
 
         # delete the customer and make sure it isn't in the database
         test_customer.delete_addresses()
->>>>>>> 36b1a2db4253041a43a21d63ce225353ebadca0d
