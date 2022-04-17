f796158a8b57779565f35212f4f2de1ebee2eea5
Allen <azflight1995@gmail.com>
2022-04-17 17:16:47 +0000

    added support for elephantsql

--- config.py
+++ config.py
@@ -2,11 +2,18 @@
 Global Configuration for Application
 """
 import os
+import json
+import logging
 
 # Get configuration from environment
 DATABASE_URI = os.getenv(
     "DATABASE_URI", "postgres://postgres:postgres@localhost:5432/postgres"
 )
+
+# override if we are running in Cloud Foundry
+if 'VCAP_SERVICES' in os.environ:
+    vcap = json.loads(os.environ['VCAP_SERVICES'])
+    DATABASE_URI = vcap['user-provided'][0]['credentials']['url']
 
 # Configure SQLAlchemy
 SQLALCHEMY_DATABASE_URI = DATABASE_URI
@@ -14,3 +21,4 @@ SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 # Secret for session management
 SECRET_KEY = os.getenv("SECRET_KEY", "s3cr3t-key-shhhh")
+LOGGING_LEVEL = logging.INFO
